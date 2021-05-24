40c66fdca95f8752fe5e6e1f2d3fa4f8bcc181c2
Jeff Cogswell <jeffcogs@gmail.com>
2021-05-24 16:51:19 -0400

    Updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from './menu-item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'menu';
+  fullmenu: MenuItem[] = [
+    { name: 'Chicken Fingers', category: 'dinner', price: 11.99 },
+    { name: 'Pizza', category: 'dinner', price: 11.99 },
+    { name: 'Wings', category: 'sides', price: 8.99 },
+    { name: 'Breadsticks', category: 'sides', price: 4.99 },
+    { name: 'Ceasar Salad', category: 'salads', price: 5.99 },
+    { name: 'Cinnamon Roll', category: 'dessert', price: 8.99 }
+  ];
 }
